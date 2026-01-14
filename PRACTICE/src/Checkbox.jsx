bbf51c400d97ab83c65e3e5294716f8d315bf6cf
Topivali-shaik <topivali09@gmail.com>
2026-01-14 11:26:55 +0530

    new concepts

---
+++ PRACTICE/src/Checkbox.jsx
@@ -0,0 +1,17 @@
+function Checkbox(){
+
+    return(
+        <div>
+            <h1>Select Your Name</h1>
+            <input type="checkbox" id="Topivali" value = "Topivali"/>
+            <label htmlFor="Topivali">TOPIVALI.SHAIK</label>
+            <br></br>
+            <input type="checkbox" id="name" value = "waseem"/>
+            <label htmlFor="name">WASEEM</label>
+        </div>
+    )
+
+
+}
+
+export default Checkbox;
